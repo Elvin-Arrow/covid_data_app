b5eb209b9e2c60861eba732ec3e2283939416508
Muhammad Talha Bin Mansoor <m.talhabinmansoor@gmail.com>
2020-07-07 17:37:38 +0500

    Update main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+///
+/// The widget housing all the records entered in the app; all while
+/// showing the require controls to add new controls and edit the
+/// existing ones
+///
 class MainScreen extends StatelessWidget {
   static const String id = "main_screen";
 
